7b35d69bc7b437e0f36db453fb86d5d99862c040
Mahadi Ahmed <mahadi4@gmail.com>
2020-12-02 08:26:59 +0100

    day 1 problem solved

---
+++ 1/expenseReport.java
@@ -0,0 +1,39 @@
+
+/**
+ * Advent of code day 1
+ */
+import java.util.*;
+import java.io.*;
+
+class expenseReport {
+
+    public static ArrayList<Integer> data(String path) throws FileNotFoundException {
+        ArrayList<Integer> input = new ArrayList<Integer>();
+        File file = new File(path);
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()) {
+            Integer n = Integer.parseInt(sc.nextLine());
+            input.add(n);
+        }
+        return input;
+    }
+
+    public static Integer sum2020(ArrayList<Integer> expenses) {
+        for (int i = 0; i < expenses.size(); i++) {
+            for (int j = 1; j < expenses.size(); j++) {
+                if (expenses.get(i) + expenses.get(j) == 2020) {
+                    return expenses.get(i) * expenses.get(j);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<Integer> expenses = data("input.txt");
+
+        Integer result = sum2020(expenses);
+        System.out.println("result: " + result);
+    }
+}
